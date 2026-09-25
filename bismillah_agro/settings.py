76f2af1529ff3b8225216a0f2bcc8131ef06d79c
almas-testcase <asaduzzaman57065@gmail.com>
2026-09-25 11:54:42 +0600

    Change database configuration to use dj_database_url
    Updated DATABASES setting to use dj_database_url for configuration.

--- bismillah_agro/settings.py
+++ bismillah_agro/settings.py
@@ -101,20 +101,20 @@ WSGI_APPLICATION = 'bismillah_agro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         conn_max_age=600,
-#         ssl_require=False
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=False
+    )
+}
 
 
 # Password validation
